6eb6961c9c00739f9f74c5d85c6a0bec06547a50
sandell <sandell@crashandlearn.se>
2017-12-13 09:36:45 +0100

    Day13 a+b

--- src/main/java/day13/Day13.java
+++ src/main/java/day13/Day13.java
@@ -9,9 +9,9 @@ class Day13 {
     static void setInput(String input) {
         layers = new HashMap<>();
         for (String row : input.split("\n")) {
-            int position = Integer.valueOf(row.split(": ")[0]);
-            int length   = Integer.valueOf(row.split(": ")[1]);
-            layers.put(position, length);
+            int pos = Integer.valueOf(row.split(": ")[0]);
+            int len = Integer.valueOf(row.split(": ")[1]);
+            layers.put(pos, len);
         }
     }
     static int getDelay() {
@@ -22,20 +22,16 @@ class Day13 {
     }
     private static boolean isCaught(int delay) {
         boolean caught = false;
-        for (int i : layers.keySet()) {
-            if ((i + delay) % (2 * layers.get(i) - 2) == 0) {
-                caught = true;
-            }
-        }
-        return caught;
+        for (int i : layers.keySet())
+            if ((i + delay) % (2 * layers.get(i) - 2) == 0)
+                return true;
+        return false;
     }
     static int getScore() {
         int sum=0;
-        for (int i : layers.keySet()) {
-            if (i % (2*layers.get(i)-2) == 0){
+        for (int i : layers.keySet())
+            if (i % (2*layers.get(i)-2) == 0)
                 sum += i*layers.get(i);
-            }
-        }
         return sum;
     }
 }
