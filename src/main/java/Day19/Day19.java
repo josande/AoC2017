eaf11a88a3d61210d7b4afda7fbd399d316a3479
sandell <sandell@crashandlearn.se>
2017-12-19 07:57:38 +0100

    19 a+b

--- src/main/java/Day19/Day19.java
+++ src/main/java/Day19/Day19.java
@@ -8,22 +8,20 @@ class Day19 {
         letters=""; steps=0;
 
         String[] rows = input.split("\n");
+
         width=rows[0].length();
         height=rows.length;
 
-        map = new char[width][height];
+        map = new char[height][width];
 
         //Create map
         for (int y=0; y<height; y++) {
-             for (int x=0; x < width; x++) {
-                 char c=rows[y].charAt(x);
-                 map[x][y]=c;
-             }
+            map[y]=rows[y].toCharArray();
         }
         //Find start pos
         y=0;
         for (x=0; x<width; x++) {
-            if (map[x][y]==('|')) {
+            if (map[y][x]==('|')) {
                 break;
             }
         }
@@ -45,30 +43,29 @@ class Day19 {
          if (x<0 || x==width || y<0 || y==height) {
              return false;
          }
-         char c = map[x][y];
+         char c = map[y][x];
          switch (c) {
              case '|' : {break;}
              case '+' : {
                  if (dy!=0) {
-                    if (x<width-1 && map[x+1][y]!=' ') {
+                    if (x<width-1 && map[y][x+1]!=' ') {
                         dy=0; dx=1;
                     }
-                    if (x>0 && map[x-1][y]!=' ') {
+                    if (x>0 && map[y][x-1]!=' ') {
                          dy=0; dx=-1;
                     }
                  } else {
-                     if (y<height-1 && map[x][y+1]!=' ') {
+                     if (y<height-1 && map[y+1][x]!=' ') {
                          dy=1; dx=0;
                      }
-                     if (y>0 && map[x][y-1]!=' ') {
+                     if (y>0 && map[y-1][x]!=' ') {
                          dy=-1; dx=0;
                      }
                  }
                  break;}
              case '-' : {break;}
-             default  : {if (c==' ')
-                 return false; letters+=c; break;}
-
+             case ' ' : {return false;}
+             default  : {letters+=c;}
          }
 
          return true;
